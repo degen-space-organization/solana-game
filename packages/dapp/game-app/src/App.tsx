78736e4b0554c943c6d3933cd22f227ae8bf877f
Luka Radisavljevic <lukaradisavljevic30@gmail.com>
2025-05-25 17:36:33 +0200

    [feat] chakra ui properly added to the project

--- packages/dapp/game-app/src/App.tsx
+++ packages/dapp/game-app/src/App.tsx
@@ -3,21 +3,43 @@ import SolanaWeb3App from './components/WalletConnection'
 import ChatExample from './components/Chat'
 import './index.css'
 
+import { Avatar, Button, Card } from "@chakra-ui/react"
 
-import { Button, HStack } from "@chakra-ui/react"
 
+
+const Demo = () => {
+  return (
+    <Card.Root width="320px">
+      <Card.Body gap="2">
+        <Avatar.Root size="lg" shape="rounded">
+          <Avatar.Image src="https://picsum.photos/200/300" />
+          <Avatar.Fallback name="Nue Camp" />
+        </Avatar.Root>
+        <Card.Title mt="2">Nue Camp</Card.Title>
+        <Card.Description>
+          This is the card body. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
+          Curabitur nec odio vel dui euismod fermentum.
+        </Card.Description>
+      </Card.Body>
+      <Card.Footer justifyContent="flex-end">
+        <Button variant="outline">View</Button>
+        <Button>Join</Button>
+      </Card.Footer>
+    </Card.Root>
+  )
+}
 
 
 function App() {
 
   return (
     <>
-      <HStack>
-        <Button>Click me</Button>
-        <Button>Click me</Button>
-      </HStack>
-      <SolanaWeb3App></SolanaWeb3App>
-      <ChatExample></ChatExample>
+      <Demo />
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+      {/* <SolanaWeb3App></SolanaWeb3App> */}
+      {/* <ChatExample></ChatExample> */}
     </>
   )
 }
--- packages/dapp/game-app/src/main.tsx
+++ packages/dapp/game-app/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from "@/components/ui/provider.tsx"
 
+// import { ChakraProvider } from '@chakra-ui/react'
+
 
 import App from './App.tsx'
 
@@ -11,7 +13,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
+    <Provider defaultTheme='light' attribute='class'>
       <App />
     </Provider>
   </StrictMode>,

4ee29b5fa00d5e00d511b5aed28c4923abad2661
Luka Radisavljevic <lukaradisavljevic30@gmail.com>
2025-06-01 04:44:22 +0200

    [chore] docker runtime; portable config for all packages; env vars

--- packages/server/src/utils/provider.ts
+++ packages/server/src/utils/provider.ts
@@ -7,8 +7,10 @@ import { Cluster, Connection, clusterApiUrl } from "@solana/web3.js";
 
 
 
+const cluster = process.env.SOLANA_NODE_ENV === 'devnet' ? 'devnet' : 'mainnet-beta';
 // const SOLANA_NETWORK: Cluster = "mainnet-beta"; // Change to "devnet" or "testnet" as needed
-const SOLANA_NETWORK: Cluster = "devnet"; // Change to "devnet" or "testnet" as needed
+// const SOLANA_NETWORK: Cluster = "devnet"; // Change to "devnet" or "testnet" as needed
+const SOLANA_NETWORK: Cluster = cluster; // Change to "devnet" or "testnet" as needed
 
 export const gSolanaProvider = new Connection(clusterApiUrl(SOLANA_NETWORK), "confirmed");
 
